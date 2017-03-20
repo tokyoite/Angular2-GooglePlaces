6da665604fd6933a732b2b841f7d9d699b4f8950
Tavi Gomez <tgomez@nevadatitle.com>
2017-03-20 15:44:28 -0400

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    property: any = {};
+    title = 'app works!';
+    place: Object = null;
+    getAddress(place: Object) {
+        this.place = place;
+        this.property.StreetAddress = place['name'];
+
+        for (let x of place["address_components"]) {
+            if (x["types"][0] == "postal_code") {
+                this.property.Zip = x["long_name"];
+            }
+
+            if (x["types"][0] == "locality") {
+                this.property.City = x["long_name"];
+            }
+
+            if (x["types"][0] == "administrative_area_level_1") {
+                this.property.State = x["long_name"];
+            }
+
+            if (x["types"][0] == "administrative_area_level_2") {
+                this.property.County = x["long_name"];
+            }
+
+            
+        }
+
+        console.log("Address Object", place);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
+﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { googleModule } from './modules/google.module';
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+      HttpModule,
+      googleModule
   ],
   providers: [],
   bootstrap: [AppComponent]

71a76fc7bfa9ccd8620b514682fc7c0a00f95591
Tavi Gomez <tavig123@hotmail.com>
2017-03-20 16:45:36 -0400

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
+    basePath: 'Angular2-GooglePlaces',
     frameworks: ['jasmine', 'angular-cli'],
     plugins: [
       require('karma-jasmine'),
